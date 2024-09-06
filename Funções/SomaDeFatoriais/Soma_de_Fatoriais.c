6d8c38416bed9c18d2cf25f1ba6278eefc7c6383
Nicolas Kleiton <inicolaskleiton@gmail.com>
2024-09-06 17:13:52 -0300

    Comentando no algoritmo 'SomaDeFatoriais' em Funções

--- Funções/SomaDeFatoriais/Soma_de_Fatoriais.c
+++ Funções/SomaDeFatoriais/Soma_de_Fatoriais.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+/**** Fatorial(): Calcula o fatorial de um número inteiro positivo
+*
+* Parâmetros:
+* n: número inteiro para o qual será calculado o fatorial
+*
+* Retorno: Retorna o valor do fatorial de 'n' como um número inteiro sem sinal de 64 bits (`unsigned long long`).
+*
+* Observações:
+* - O fatorial de um número é o produto de todos os inteiros de 1 até 'n' (n! = 1 * 2 * 3 * ... * n).
+* - A função retorna 1 para n = 0, pois 0! = 1 por definição.
+* - Devido ao rápido crescimento do fatorial, a função utiliza `unsigned long long` para permitir armazenar números grandes.
+****/
+
 unsigned long long Fatorial(int n){
     unsigned long long fatorial = 1;
    
